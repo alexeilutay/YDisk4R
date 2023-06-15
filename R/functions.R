fe9f08af4f65257f0ded65345a537b56e4f990fe
Alexei Lutay <alexei.lutay@yandex.ru>
2023-06-15 21:58:43 +0300

    refactoring the code

--- R/functions.R
+++ R/functions.R
@@ -2,7 +2,7 @@ utils::globalVariables(c("type"))
 
 #' Checks for YDisk token in the Global Environment
 #'
-#' @description `set_YD_oath` tries to find Yandex Disk OAuth token in Global Environment
+#' @description `set_YD_oauth` tries to find Yandex Disk OAuth token in Global Environment
 #'
 #' @details
 #' First, the function seeks the YDisk in Global Environment. If YDisk is absent in GE,
@@ -12,22 +12,22 @@ utils::globalVariables(c("type"))
 #' @export
 #' @examples
 #'\dontrun{
-#' set_YD_oath()
+#' set_YD_oauth()
 #' }
-set_YD_oath <- function(){
-  ydoath <- Sys.getenv("YDisk")
-  if(ydoath != ""){
+set_YD_oauth <- function(){
+  ydoauth <- Sys.getenv("YDisk")
+  if(ydoauth != ""){
     print("YDisk is found in Global Environment and will be used as OAuth token for Yandex Disk")
-    return(ydoath)
+    return(ydoauth)
   } else {
     genv <- Sys.getenv()[which(grepl("^OAuth y0_", Sys.getenv()))]
     if(!purrr::is_empty(genv)){
-      ydoath <- as.character(genv)
+      ydoauth <- as.character(genv)
       YD_cand <- names(genv)
       cat(paste0("YDisk is not set in Global Environment, but ",
                    YD_cand, " variable starts with OAuth y0_... and is probably ",
                    "how you store Yandex Disk OAuth token. You can run Sys.getenv(",YD_cand,") and check if this is true."))
-      return(ydoath)
+      return(ydoauth)
     } else {
       cat(paste0('We found nothing resembling OAuth token in Global Environment. ',
                    'To use this package further, you may set OAuth manually as variable, ',
@@ -51,32 +51,28 @@ set_YD_oath <- function(){
 #' the function return an empty tibble.
 #'
 #' @param path a path at YDisk, it should start with disk:/
-#' @param YD_oath an OAuth token for YDisk
+#' @param token an OAuth token for YDisk
 #' @param limit how many folders to return, by default 25
 #' @return a tibble
 #' @export
 #' @examples
 #'\dontrun{
-#' get_YD_folders(YD_oath = set_YD_oath())
-#' get_YD_folders(path = "disk:/Загрузки/", limit = 5, YD_oath = set_YD_oath())
+#' yd_oauth <- set_YD_oauth()
+#' get_YD_folders(token = yd_oauth)
+#' get_YD_folders(path = "disk:/Загрузки/", limit = 5, token = set_YD_oauth())
 #' }
-get_YD_folders <- function(path = "disk:/", limit = 25, YD_oath){
+get_YD_folders <- function(path = "disk:/", limit = 25, token){
   if(grepl("^disk:\\/", path)){
     folders <- paste0("https://cloud-api.yandex.net/v1/disk/resources",
                       "?path=", path ,"&limit=", limit) |>
       httr::GET(httr::add_headers(Accept = "application/json",
-                                  Authorization = YD_oath)) |>
+                                  Authorization = token)) |>
       httr::content() |>
       purrr::pluck("_embedded") |>
-      purrr::pluck("items")  |>
-      purrr::map_df(
-        ~dplyr::tibble(
-          name = .x |> purrr::pluck("name"),
-          type = .x |> purrr::pluck("type"),
-          path = .x |> purrr::pluck("path")
-          )
-        ) |>
-      dplyr::filter(type=="dir")
+      purrr::pluck("items")
+
+    folders <- Filter(function(x) x$type == "dir", folders) |>
+          purrr::map_df(~.x %>% purrr::flatten())
 
     if(nrow(folders)==0){
       print("looks like at the path provided there are no folders")
@@ -96,46 +92,45 @@ get_YD_folders <- function(path = "disk:/", limit = 25, YD_oath){
 #' @import purrr
 #' @import dplyr
 #' @import stringr
-#'
 #' @details
 #' The path preset by default is disk:/ (root folder of your YDisk),
 #' set own path starting with disk:/. If there are no folders at the indicated path,
 #' the function return an empty tibble.
 #'
 #' @param path a path at YDisk, it should start with disk:/
-#' @param YD_oath an OAuth token for YDisk
+#' @param token an OAuth token for YDisk
 #' @param limit how many files to return, by default 25
 #' @return a tibble
 #' @export
 #' @examples
 #'\dontrun{
-#' get_YD_files(YD_oath = set_YD_oath())
-#' get_YD_files(path = "disk:/Загрузки/", limit = 100, YD_oath = set_YD_oath())
+#' yd_oauth <- set_YD_oauth()
+#' get_YD_files(token = set_YD_oauth())
+#' get_YD_files(path = "disk:/Загрузки/", limit = 100, token = yd_oauth)
 #' }
-get_YD_files <- function(path = "disk:/", limit = 25, YD_oath){
+get_YD_files <- function(path = "disk:/", limit = 25, token){
   if(grepl("^disk:\\/", path)){
-    files <- paste0("https://cloud-api.yandex.net/v1/disk/resources",
+    xfiles <- paste0("https://cloud-api.yandex.net/v1/disk/resources",
                       "?path=", path ,"&limit=", limit) |>
       httr::GET(httr::add_headers(Accept = "application/json",
-                                  Authorization = YD_oath)) |>
+                                  Authorization = token)) |>
       httr::content() |>
       purrr::pluck("_embedded") |>
-      purrr::pluck("items")  |>
-      purrr::map_df(
-        ~dplyr::tibble(
-          name = .x |> purrr::pluck("name"),
-          type = .x |> purrr::pluck("type"),
-          yd_size = .x |> purrr::pluck("size"),
-          yd_created = .x |> purrr::pluck("created") |>
-            as.character() |> stringr::str_extract("\\d{4}-\\d{2}-\\d{2}"),
-          file = .x |> purrr::pluck("file")
-          )
-        )
+      purrr::pluck("items")
 
-    if(nrow(files)==0){
+    xfiles <-  Filter(function(x) x$type == "file", xfiles) |>
+      purrr::map_df(~.x %>%
+              purrr::modify_at("sizes",
+                            \(x) purrr::map(x, ~paste(.x$name, "::", .x$url))) %>%
+              purrr::modify_at("sizes",
+                            \(x) paste(x, collapse = " | ")) %>%
+                purrr::flatten()
+              )
+
+    if(nrow(xfiles)==0){
       print("looks like there are no files at the provided path")
     }
-    return(files)
+    return(xfiles)
   } else {
     print("the path should start with disk:/")
     return(NULL)
@@ -154,30 +149,33 @@ get_YD_files <- function(path = "disk:/", limit = 25, YD_oath){
 #' @param path path to folder where to upload, by default is disk:/ (should start with disk:/).
 #' @param yd_fname the filename to use for the uploaded file at Yandex disk. If not set, it is the same as the disk_fname.
 #' @param disk_fname the filename (to be uploaded) on your hard disk
-#' @param YD_oath an OAuth token for YDisk
+#' @param token an OAuth token for YDisk
 #' @param overwrite if the file should be overwritten (by defaul = FALSE)
 #'
 #' @export
 #'
 #' @examples
 #'\dontrun{
+#'yd_oauth <- set_YD_oauth()
 #'upload_file_2YD(path = "disk:/Загрузки/", disk_fname = "../../bookmarks.html",
-#'                overwrite = TRUE, YD_oath = set_YD_oath())
+#'                overwrite = TRUE, token = yd_oauth)
 #'}
 upload_file_2YD <- function(path = "disk:/", yd_fname = NULL, disk_fname,
-                            overwrite=FALSE, YD_oath){
-  if(is.null(yd_fname)){ yd_fname <- stringr::str_extract(disk_fname, "[^\\/]+$") }
+                            overwrite=FALSE, token){
+  if(is.null(yd_fname)){
+    yd_fname <- stringr::str_extract(disk_fname, "[^\\/]+$")
+    }
   xref <- httr::GET(paste0("https://cloud-api.yandex.net/v1/disk/resources/upload",
                      "?path=", paste0(path, yd_fname),
                      "&overwrite=", overwrite,
                      "&fields=name,_embedded.items.path"),
               httr::add_headers(Accept = "application/json",
-                          Authorization = YD_oath)) |>
+                          Authorization = token)) |>
     httr::content() |>
     purrr::pluck("href")
+  print("Uploading is being started. Check the status: 201 would mean the file's been uploaded")
   httr::PUT(url = xref,
             body = upload_file(disk_fname))
-  print("the status 201 means the file has been uploaded")
 }
 
 
@@ -190,28 +188,25 @@ upload_file_2YD <- function(path = "disk:/", yd_fname = NULL, disk_fname,
 #' The function returns a dataframe with all the details for your public files
 #'
 #' @param limit how many files to return, by default 100
-#' @param YD_oath an OAuth token for YDisk
+#' @param token an OAuth token for YDisk
 #' @return a dataframe
 #' @export
 #'
 #' @examples
-#' my_public_files(limit = 1000, YD_oath = set_YD_oath())
-my_public_files <- function(limit = 100, YD_oath){
+#' my_public_files(limit = 1000, token = set_YD_oauth())
+my_public_files <- function(limit = 100, token){
   xfiles <- paste0("https://cloud-api.yandex.net/v1/disk/resources/public?limit=", limit) |>
-    httr::GET(httr::add_headers(Accept = "application/json", Authorization = YD_oath)) |>
+    httr::GET(httr::add_headers(Accept = "application/json",
+                                Authorization = token)) |>
     httr::content() |>
-    purrr::pluck("items")
-
-  check <- function(x,name){
-    # to remove the elements named `sizes` (ref. https://stackoverflow.com/a/54777781)
-    m = names(x) %in% name
-    x = if(any(m)) x[!m] else x
-    if(is.list(x)) sapply(x,check,name)
-    else x
-  }
-
-  my_public_files_df <- xfiles |>
-    purrr::map_df(~check(.x, "sizes") |> purrr::flatten())
-  return(my_public_files_df)
+    purrr::pluck("items") |>
+    purrr::map_df(~.x %>%
+                    purrr::modify_at("sizes",
+                                     \(x) purrr::map(x, ~paste(.x$name, "::", .x$url))) %>%
+                    purrr::modify_at("sizes",
+                                     \(x) paste(x, collapse = " | ")) %>%
+                    purrr::flatten()
+    )
+  return(xfiles)
 }
 
